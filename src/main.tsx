627c19c645dcd99622e16626e792051dff3ca665
Aakash Shah <appsparkler@gmail.com>
2024-03-30 16:40:07 +0530

    chore: working programmatic prompting for installing app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import "./index.css";
+
+let deferredPrompt: (Event & { prompt: () => Promise<void> }) | null = null;
+
+deferPrompt();
+
+const InstallButton = () => {
+  const handleClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt()
+    }
+  };
+  return (
+    <>
+      <button onClick={handleClick}>
+        <h1>Install</h1>
+      </button>
+      ;
+    </>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <InstallButton />
   </React.StrictMode>
 );
 
@@ -13,4 +34,12 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/sw.js")
     .then(() => console.log("service worker registered"));
+}
+
+function deferPrompt() {
+  window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    console.log("before install prevented...");
+    deferredPrompt = event;
+  });
 }
