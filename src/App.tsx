acad86686af08b475eca578f02308c9476e1184e
Aakash Shah <appsparkler@gmail.com>
2024-03-31 15:15:22 +0530

    chore: prepare for caching

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [src, setSrc] = useState<null | string>(null);
+
+  useEffect(() => {
+    fetch("https://httpbin.org/get").then(() => {
+      setSrc(
+        "https://images.unsplash.com/photo-1597362925123-77861d3fbac7?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      );
+    });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={"/vite.svg"} width={100} />
+      <div>{src !== null ? <img src={src} width="100%" /> : "Loading..."}</div>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
+import App from "./App.tsx";
 import "./index.css";
-import { InstallButton } from "./InstallButton";
+// import { InstallButton } from "./InstallButton";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <InstallButton />
+    <App />
+    {/* <InstallButton /> */}
   </React.StrictMode>
 );
 
