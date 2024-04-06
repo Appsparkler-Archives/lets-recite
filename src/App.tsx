03f7b1a2f67ac6fbc2850b545d552db130543b9c
Aakash Shah <appsparkler@gmail.com>
2024-04-06 16:43:53 +0530

    chore: wip

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
   };
 
   const handleClickEnableNotifications = () => {
-    // notifyMe();
+    askForNotificationPermission();
   };
 
   useEffect(() => {
@@ -67,6 +67,110 @@ function App() {
       ))}
     </Box>
   );
+}
+
+function configurePushSub() {
+  const hasServiceWorker = "serviceWorker" in navigator;
+  let reg: ServiceWorkerRegistration;
+  if (hasServiceWorker) {
+    navigator.serviceWorker.ready
+      .then((swreg) => {
+        reg = swreg;
+        return swreg.pushManager.getSubscription();
+      })
+      .then((sub) => {
+        if (sub === null) {
+          const vapidPublicKey =
+            "BCvIkKpShJ56bdwthQ4IesqvQLMRR6PibI6Ra83-acopiBXHsZAtvtha5dBY6Cuexcq5bMtDLevF5HLsbKpYx0A";
+          const convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+          return reg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: convertedVapidPublicKey,
+          });
+        }
+      })
+      .then((newSub) => {
+        if (newSub) {
+          return fetch(
+            "https://learningpwa-610f0-default-rtdb.asia-southeast1.firebasedatabase.app/subscriptions.json",
+            {
+              method: "POST",
+              body: JSON.stringify(newSub),
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+            }
+          );
+        }
+      })
+      .then((res) => res?.json())
+      .then((res) => {
+        if (res.ok) {
+          displayConfirmNotification();
+        }
+      })
+      .catch((err) => {
+        console.error("Could not subscribe to notifications", err);
+      });
+  }
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function (result) {
+    console.log("User Choice", result);
+    if (result !== "granted") {
+      console.log("No notification permission granted!");
+    } else {
+      configurePushSub();
+      // displayConfirmNotification();
+    }
+  });
+}
+
+function displayConfirmNotification() {
+  if ("serviceWorker" in navigator) {
+    const options: NotificationOptions = {
+      body: "You successfully subscribed to our Notification service!",
+      icon: "/src/images/icons/app-icon-96x96.png",
+      image: "/src/images/sf-boat.jpg",
+      dir: "ltr",
+      lang: "en-US", // BCP 47,
+      vibrate: [100, 50, 200],
+      badge: "/src/images/icons/app-icon-96x96.png",
+      tag: "confirm-notification",
+      renotify: true,
+      actions: [
+        {
+          action: "confirm",
+          title: "Okay",
+          icon: "/src/images/icons/app-icon-96x96.png",
+        },
+        {
+          action: "cancel",
+          title: "Cancel",
+          icon: "/src/images/icons/app-icon-96x96.png",
+        },
+      ],
+    };
+
+    navigator.serviceWorker.ready.then(function (swreg) {
+      swreg.showNotification("Successfully subscribed!", options);
+    });
+  }
+}
+
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
 
 export default App;
